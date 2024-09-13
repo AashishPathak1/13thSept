cd19e42067485431ecfa1c59d0bd56c5d3c15580
AASHISH PATHAK <mr.aashishpathak.official@gmail.com>
2024-09-13 12:46:24 +0530

    pushing the chnged code

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -7,6 +7,14 @@ function getAllUsers(req, res) {
   } catch (err) {}
 }
 
+function getUser(req, res) {
+  try {
+    let id = parseInt(req.params.id);
+    let user = users.find((u) => u.id === id);
+    res.json(user);
+  } catch (err) {}
+}
 module.exports = {
   getAllUsers,
+  getUser
 };
